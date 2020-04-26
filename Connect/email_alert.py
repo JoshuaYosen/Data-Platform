cffc50be202ed5994ed5a9f75f3ab1ed6e29f654
Joshua_Yosen <joshuayosen@gmail.com>
2020-04-26 10:08:25 -0400

    refactor email_alert.py

--- Connect/email_alert.py
+++ Connect/email_alert.py
@@ -1,6 +1,26 @@
+from datetime import date
+import os.path
 import yagmail
 
-contents = ["test"]
+def email(path, from, to):
+    date = date.today()
+    year = date.strftime("%Y")
+    if os.path.exists(path + '{}_survey.csv'.format(year)):
+        contents = ["The {} Survey has been downloaded!".format(year)]
+        return contents
+
+    else:
+        contents = ["No new surveys available :("]
+        return contents
+
+    yagmail.SMTP(from).send(to, '***SO Survey Download Alert***', contents)
 
 
-yagmail.SMTP('joshuayosen@gmail.com').send('joshuayosen@gmail.com', '**Stack Overflow Download Alert**', contents)
+
+
+
+if __name__ == "__main__":
+    path = 'home/user/Project/Surveys/'
+    from = 'my_secret_mail@gmail.com'
+    to = 'my_developer_mail@gmail.com'
+    email(path, from, to)
